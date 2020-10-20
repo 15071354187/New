5070928a9da4a8b826150361d9f6fdbc3dd5239e
xuming <745289524@qq.com>
2020-10-20 16:37:44 +0800

    test

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Soul
+ * Date: 2020/10/20
+ */
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
